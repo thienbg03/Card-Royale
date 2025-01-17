084b486005f0d6fca03617ec69e2429b02ded5fa
thienbg03 <bdan2300@gmail.com>
2025-01-17 18:00:20 -0500

    Added Start Menu, Boss Model, Player Model
    - Start Menu has a play button, allow transition to level 1. The play button changes colours when hover.
    - Imported the player and the enemy bull boss models

---
+++ Assets/Code/StartMenu/PlayButton.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PlayButton : MonoBehaviour
+{
+    private Image backgroundImage;
+    private Text text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        backgroundImage = GetComponent<Image>();
+        text = GetComponentInChildren<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MouseHover()
+    {
+        print("OnMouseEnter");
+        backgroundImage.color = Color.white;
+        text.color = new Color(0.1568628f, 0.1568628f, 0.1568628f);
+    }
+
+    public void MouseExit()
+    {
+        text.color = Color.white;
+        backgroundImage.color = new Color(0.1568628f, 0.1568628f, 0.1568628f);
+    }
+
+
+    public void MouseClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
